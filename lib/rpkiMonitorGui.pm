2545bdfb07691610e3fad79dfc266cae09ab5759
Wes Hardaker <opensource@hardakers.net>
2016-02-25 15:37:16 -0800

    flushed out rcynic info

--- lib/rpkiMonitorGui.pm
+++ lib/rpkiMonitorGui.pm
@@ -10,7 +10,7 @@ get '/' => sub {
 
 get '/rpki-rtr-client-monitor' => sub {
 
-	my %vars = (
+	my %data = (
 	            date => '2016-02-15 23:11:05',
 	            sources => [
 	                        ['ipv4::141.22.28.222::23072',  'INTEGER: up(1)'],
@@ -23,7 +23,50 @@ get '/rpki-rtr-client-monitor' => sub {
 	                          }
 	           );
 	
-	return template 'rpki-rtr-client-monitor' => \%vars;
+	return template 'rpki-rtr-client-monitor' => \%data;
+};
+
+get '/rcynic-status' => sub {
+	my %data = (
+	            ROAInformation =>
+	            {
+	             ROAsCurrent => 4224,
+	             ROAsAdded => 4224,
+	             KeysCurrent => 0,
+	             KeysAdded => 0,
+	            },
+
+	            WarningInfo =>
+	            {
+	             FailedValidation => 2,
+	             MissingObjects => 2,
+	             NotSubset => 2,
+	             Rejected => 2,
+	             CPS => 361,
+	             StaleCRL => 2161,
+	             StaleManifest => 2163,
+	            },
+
+	            DataSources =>
+	            [
+	               ['202.30.51.27', 'Yes'],
+                       ['ca0.rpki.net', 'No'],
+                 ['localcert.ripe.net', 'No'],
+                    ['localhost:10873', 'Yes'],
+                               ['main', 'Yes'],
+                     ['repo0.rpki.net', 'Yes'],
+              ['repository.lacnic.net', 'No'],
+                   ['rpki-02.rarc.net', 'Yes'],
+          ['rpki-repository.nic.ad.jp', 'Yes'],
+             ['rpki-testbed.apnic.net', 'No'],
+                   ['rpki.afrinic.net', 'No'],
+                     ['rpki.apnic.net', 'No'],
+                      ['rpki.rarc.net', 'Yes'],
+	              ['rpki.ripe.net', 'Yes']
+	            ]
+	           );	            
+
+	return template 'rcynic-status' => \%data;
 };
 
 true;
