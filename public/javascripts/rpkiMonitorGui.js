ba3bc402392638cb530384457029a05f8ce8a753
Wes Hardaker <opensource@hardakers.net>
2016-03-01 14:40:09 -0800

    handle multiple caches, all in one table at the moment.

--- public/javascripts/rpkiMonitorGui.js
+++ public/javascripts/rpkiMonitorGui.js
@@ -1,18 +1,31 @@
 function rpkiLoadFrames(fromurl, framesToUpdate, options) {
     var compiledSources = {};
+
+    function debug(info) {
+	if (options.debug) {
+	    console.log(info)
+	}
+    }
+
     if (options && 'reloadIconId' in options) {
 	$("." + options['reloadIconId']).css('visibility','visible');
     }
+
+    debug("fetching " + fromurl);
     $.ajax({
 	url:  fromurl,
 	type: 'GET',
 	success: function(result) {
+	    debug("fetch successful");
+
 	    // note: not sure why this is getting on object instead of a string
 	    // that needs to be passed to JSON.parse() first
 
 	    for(var i = 0; i < framesToUpdate.length; i++) {
 		var sourceid = framesToUpdate[i]['source'];
 		var frameid = framesToUpdate[i]['frame'];
+
+		debug("  filling out " + frameid + "  based on " + sourceid);
 		var sourceFrame = $("#" + sourceid).html();
 		if (! sourceFrame) {
 		    alert("error in web page ; see console for technical details");
@@ -20,7 +33,7 @@ function rpkiLoadFrames(fromurl, framesToUpdate, options) {
 		    return;
 		}
 		compiledSources[sourceid] = Handlebars.compile(sourceFrame);
-		
+
 		if (! compiledSources[sourceid]) {
 		    alert("failed to load some information from the server");
 		    console.log("handlebars failed to compile");
@@ -29,7 +42,10 @@ function rpkiLoadFrames(fromurl, framesToUpdate, options) {
 		var htmlOutput = compiledSources[sourceid](result);
 		
 		if (htmlOutput) {
+		    debug("got to inserting html into " + frameid);
 		    $('#' + frameid).html(htmlOutput);
+		    debug("inserting: " + htmlOutput);
+		    debug($('#' + frameid));
 		} else {
 		    alert("failed to decode some data from the server");
 		}
