20fa696da46316fbca9c186eb8a428fd3b55d278
Michael <baerm@rebma.mikesoffice.com>
2016-03-15 14:42:03 -0700

    some code cleanup, added some test data

--- lib/rpkiMonitorGui.pm
+++ lib/rpkiMonitorGui.pm
@@ -35,97 +35,13 @@ get '/rpki-rtr-client-monitor' => sub {
 
 get '/rcynic-status-data' => sub {
     my %config = (
-	rcynicl => "/home/baerm/wtemp/rcynic/lrcynic.xml",
-	rcynicr => "/home/baerm/wtemp/rcynic/rrcynic.xml"
+	rcynicl => "rcynic-data/lrcynic.xml",
+	rcynicr => "rcynic-data/rrcynic.xml"
 	);
 
     my $data = rpkiRtrRcynicStatus::rpkiRtrRcynicStatus_getData(\%config);
 
-    my %tdata = (
-	'rcynic1' =>
-	{
-	    ROAInformation =>
-	    {
-		ROAsCurrent => int(rand 10000),
-		ROAsAdded => 4224,
-		KeysCurrent => 0,
-		KeysAdded => 0,
-	    },
-
-	    WarningInfo =>
-	    {
-		FailedValidation => 2,
-		MissingObjects => 2,
-		NotSubset => 2,
-		Rejected => 2,
-		CPS => 361,
-		StaleCRL => 2161,
-		StaleManifest => 2163,
-	    },
-
-	    DataSources =>
-		[
-		 ['202.30.51.27', 'Yes'],
-		 ['ca0.rpki.net', 'No'],
-		 ['localcert.ripe.net', 'No'],
-		 ['localhost:10873', 'Yes'],
-		 ['main', 'Yes'],
-		 ['repo0.rpki.net', 'Yes'],
-		 ['repository.lacnic.net', 'No'],
-		 ['rpki-02.rarc.net', 'Yes'],
-		 ['rpki-repository.nic.ad.jp', 'Yes'],
-		 ['rpki-testbed.apnic.net', 'No'],
-		 ['rpki.afrinic.net', 'No'],
-		 ['rpki.apnic.net', 'No'],
-		 ['rpki.rarc.net', 'Yes'],
-		 ['rpki.ripe.net', 'Yes']
-		]
-	},
-
-	'cache2' =>
-	{
-	    ROAInformation =>
-	    {
-		ROAsCurrent => 0 - int(rand 10000),
-		ROAsAdded => 4224,
-		KeysCurrent => 0,
-		KeysAdded => 0,
-	    },
-
-	    WarningInfo =>
-	    {
-		FailedValidation => 2,
-		MissingObjects => 2,
-		NotSubset => 2,
-		Rejected => 2,
-		CPS => 361,
-		StaleCRL => 2161,
-		StaleManifest => 2163,
-	    },
-
-	    DataSources =>
-		[
-		 ['202.30.51.27', 'Yes'],
-		 ['ca0.rpki.net', 'No'],
-		 ['localcert.ripe.net', 'No'],
-		 ['localhost:10873', 'Yes'],
-		 ['main', 'Yes'],
-		 ['repo0.rpki.net', 'Yes'],
-		 ['repository.lacnic.net', 'No'],
-		 ['rpki-02.rarc.net', 'Yes'],
-		 ['rpki-repository.nic.ad.jp', 'Yes'],
-		 ['rpki-testbed.apnic.net', 'No'],
-		 ['rpki.afrinic.net', 'No'],
-		 ['rpki.apnic.net', 'No'],
-		 ['rpki.rarc.net', 'Yes'],
-		 ['rpki.ripe.net', 'Yes']
-		]
-	},
-	
-	);	            
-
     return $data;
-#    return \%tdata;
 };
 
 get '/rcynic-status' => sub {
